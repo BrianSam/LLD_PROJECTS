ee4cb940b2b4a56c10448720c02c9dd34045de75
BrianSam <briansamgames@gmail.com>
2024-04-16 19:45:13 +0530

    tictactoe_1st

---
+++ src/TicTacToe/stratergies/botPlayingStratergies/EasyBotPlayingStratergy.java
@@ -0,0 +1,11 @@
+package TicTacToe.stratergies.botPlayingStratergies;
+
+import TicTacToe.models.Board;
+import TicTacToe.models.Move;
+
+public class EasyBotPlayingStratergy implements BotPlayingStratergies{
+    @Override
+    public Move makeMove(Board board) {
+        return null;
+    }
+}
---
+++ src/TicTacToe/stratergies/botPlayingStratergies/HardBotPlayingStratergy.java
@@ -0,0 +1,11 @@
+package TicTacToe.stratergies.botPlayingStratergies;
+
+import TicTacToe.models.Board;
+import TicTacToe.models.Move;
+
+public class HardBotPlayingStratergy implements BotPlayingStratergies{
+    @Override
+    public Move makeMove(Board board) {
+        return null;
+    }
+}
